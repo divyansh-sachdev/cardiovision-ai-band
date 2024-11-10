8458f1d335f46833cb3930ba9d065e8edc4c221c
Divyansh Sachdev <89719842+divyansh-sachdev@users.noreply.github.com>
2024-11-10 11:20:00 +0530

    Initial commit: AD8232 ECG analog front-end and bandpass filtering

---
+++ config.example.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ECG_SAMPLE_RATE     250 // Hz
+#define ECG_ANALOG_PIN      36  // ADC1_CH0
+#define LO_PLUS_PIN         32  // Leads-off detect +
+#define LO_MINUS_PIN        33  // Leads-off detect -
+
+#define TACHYCARDIA_BPM     110
+#define BRADYCARDIA_BPM     45
+
+#endif
